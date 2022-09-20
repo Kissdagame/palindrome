31725320b2c3431bb9ca9682b4d8def28ad9da04
kissdagame <kissdagame@outlook.com>
2022-09-20 09:32:30 -0700

    Add project files.

---
+++ palindrome/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace palindrome
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a word and press enter");
+
+            string input = Console.ReadLine();
+            Console.WriteLine(TestString(input));
+        }
+
+        private static bool TestString(string input)
+        {
+            char[] chars = input.ToCharArray();
+            for (int i = 0; i < chars.Length; i++)
+            {
+                if (chars[i] != chars[chars.Length - 1 - i])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
